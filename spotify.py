15f2ada43f3ecf163f6edbb6deb20f26e7a6f01a
Appuchia <46938448+appuchias@users.noreply.github.com>
2020-12-24 19:46:10 +0100

    Get songs as a list of tuples

--- spotify.py
+++ spotify.py
@@ -1,12 +1,19 @@
+# pylint: disable=wildcard-import, missing-module-docstring
 import tekore as tk
+from pprint import pprint
 
 client_id, client_secret, redirect_uri = tk.config_from_file(".env")
 
-token = tk.request_client_token(client_id, client_secret) # // tk.prompt_for_user_token(client_id, client_secret, redirect_uri)
+token = tk.request_client_token(client_id, client_secret)
+# token = tk.prompt_for_user_token(client_id, client_secret, redirect_uri)
 
 sp = tk.Spotify(token)
 
 user_id = input("User ID:\n> ")
-playlists = sp.playlists(user_id)
+playlists = sp.playlists(user_id).items  # pylint: disable=E1101
 
-playlists = playlists.items
+simple_playlist = playlists[0]
+
+full_playlist = [track.track for track in sp.playlist(playlist_id=simple_playlist.id).tracks.items]
+
+tracks = [(track.name, [artist.name for artist in track.artists]) for track in full_playlist]
