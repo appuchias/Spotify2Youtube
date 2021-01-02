f7b09224012f16fbbfd7e02c75d170f3da1fb5d5
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-01-02 03:18:26 +0100

    Folder rename

--- Spotify2Youtube/settings.py
+++ Spotify2Youtube/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(
+    "C:\\V\\Programming\\Spotify2Youtube\\"
+).resolve()  # Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -118,5 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_ROOT = "staticfiles/"
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
