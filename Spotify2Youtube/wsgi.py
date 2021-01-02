a9348f336f4e74a956d8174782e2fa26a7577469
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-01-02 03:48:01 +0100

    Fix "Spoitify" to "Spotify"

--- Spotify2Youtube/wsgi.py
+++ Spotify2Youtube/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Spoitify2Youtube project.
+WSGI config for Spotify2Youtube project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Spoitify2Youtube.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Spotify2Youtube.settings")
 
 application = get_wsgi_application()
