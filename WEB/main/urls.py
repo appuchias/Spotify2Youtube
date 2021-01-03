fbd65fcc7052a19cb185e8cab8032d03e440e3cf
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-01-03 13:52:46 +0100

    Remove web

--- WEB/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home),
-    path("home/", views.home),
-    path("spotify/", views.spotify),
-    path("404/", views._404),
-]
