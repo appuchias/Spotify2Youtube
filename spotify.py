1b4b9355672839b0b000c49164409835c47caaa0
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-01-06 16:37:01 +0100

    Reformat to function

--- spotify.py
+++ spotify.py
@@ -6,18 +6,27 @@ client_id, client_secret, redirect_uri = tk.config_from_file(".env")
 token = tk.request_client_token(client_id, client_secret)
 # token = tk.prompt_for_user_token(client_id, client_secret, redirect_uri)
 
-sp = tk.Spotify(token)
 
-user_id = input("User ID:\n> ")
-playlists = sp.playlists(user_id).items  # pylint: disable=E1101
+def main():
+    sp = tk.Spotify(token)
 
-p_id = playlists[0].id
+    user_id = input("User ID:\n> ")
+    playlists = sp.playlists(user_id).items  # pylint: disable=E1101
 
-full_playlist = [
-    track.track
-    for track in sp.playlist(playlist_id=p_id).tracks.items  # pylint: disable=E1101
-]
+    p_id = playlists[0].id
 
-tracks = [
-    (track.name, [artist.name for artist in track.artists]) for track in full_playlist
-]
+    full_playlist = [
+        track.track
+        for track in sp.playlist(playlist_id=p_id).tracks.items  # pylint: disable=E1101
+    ]
+
+    tracks = [
+        (track.name, [artist.name for artist in track.artists])
+        for track in full_playlist
+    ]
+
+    return tracks
+
+
+if __name__ == "__main__":
+    main()
