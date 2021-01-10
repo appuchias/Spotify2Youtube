e04bb54e366e716c53eb217fc88f7fa7d83a28ee
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-01-10 23:14:37 +0100

    Run with specific playlist id

--- spotify.py
+++ spotify.py
@@ -2,30 +2,29 @@
 import tekore as tk
 
 
-def main():
-    client_id, client_secret, _redirect_uri = tk.config_from_file(".env")
+def spotify(pid: str = None):
+    assert pid, "No playlist specified"
 
+    client_id, client_secret, _redirect_uri = tk.config_from_file(".env")
     token = tk.request_client_token(client_id, client_secret)
     # token = tk.prompt_for_user_token(client_id, client_secret, _redirect_uri)
 
     sp = tk.Spotify(token)
 
-    user_id = input("User ID:\n> ")
-    playlists = sp.playlists(user_id).items
-
-    p_id = playlists[0].id
+    playlist = sp.playlist(playlist_id=pid)
 
     full_playlist = [
-        track.track for track in sp.playlist(playlist_id=p_id).tracks.items
-    ]  # [track1(obj), track2(obj)]
+        track.track for track in playlist.tracks.items
+    ]
 
     tracks = [
         (track.name, [artist.name for artist in track.artists])
         for track in full_playlist
-    ]  # [(song1, [artist1, artist2]), (song2, [artist3, artist4])]
+    ]
 
-    return tracks
+    # [(song1, [artist1, artist2]), (song2, [artist3, artist4])]
+    return (tracks, playlist.name)
 
 
 if __name__ == "__main__":
-    print(main())
+    print(spotify())
