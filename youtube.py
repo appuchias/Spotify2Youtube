896e5d203c6e1fb30864b9dc822d394f7ded4903
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-01-10 23:14:44 +0100

    Done

--- youtube.py
+++ youtube.py
@@ -1,7 +1,6 @@
 # pylint: disable=no-member
 import os
-
-from pprint import pprint
+from time import sleep
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
@@ -10,47 +9,74 @@ import googleapiclient.errors
 scopes = ["https://www.googleapis.com/auth/youtube"]
 
 
-def main(videos_q: list = None):
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+def youtube(songs_q: list = [], pname: str = "Playlist by API"):
+    # General values
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # 0 if public
 
-    api_service_name = "youtube"
-    api_version = "v3"
+    api_service_name, api_version = "youtube", "v3"
     client_secrets_file = "client_secret.json"
 
     # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    credentials = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
         client_secrets_file, scopes
-    )
-    credentials = flow.run_console()
+    ).run_console()
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials
     )
 
-    playlist_create = youtube.playlists().insert(
-        part="snippet,status",
-        body={
-            "snippet": {
-                "title": "Sample playlist created via API",
-                "description": "This is a sample playlist description.",
-                "tags": ["sample playlist", "API call"],
-                "defaultLanguage": "en",
+    # Actual valuable traffic
+    playlist_id = (
+        youtube.playlists()
+        .insert(
+            part="snippet,status",
+            body={
+                "snippet": {
+                    "title": pname,
+                    "description": "Playlist from Spotify",
+                    "tags": ["By Appu", "Spotify"],
+                    "defaultLanguage": "es",
+                },
+                "status": {"privacyStatus": "private"},
             },
-            "status": {"privacyStatus": "private"},
-        },
-    )
+        )
+        .execute()
+    )["id"]
 
-    playlist = playlist_create.execute()
-    playlist_id = playlist["id"]
+    for song in songs_q:
+        video = (
+            youtube.search()
+            .list(part="snippet", maxResults=1, q=f"{song[0]}, {', '.join(song[1])}")
+            .execute()["items"]
+        )[0]
 
-    videos_dict = (
-        youtube.search().list(part="snippet", maxResults=5, q="surfing").execute()
-    )
+        # print([video["snippet"]["title"] for video in videos])
 
-    videos = videos_dict["items"]
+        # for video in videos:
+        youtube.playlistItems().insert(
+            part="snippet",
+            body={
+                "snippet": {
+                    "playlistId": playlist_id,
+                    "position": 0,
+                    "resourceId": {
+                        "kind": "youtube#video",
+                        "videoId": video["id"]["videoId"],
+                    },
+                }
+            },
+        ).execute()
 
-    pprint(videos)
-    pprint(playlist_id)
+        print(f" - Added {video['snippet']['title']} to the playlist\n")
+
+        sleep(0.5)
+
+    print("\nDone adding songs")
 
 
 if __name__ == "__main__":
-    main()
+    youtube(
+        [
+            ("I'll come back to you", ["Rxseboy", "Powfu"]),
+            ("Throw it all away", ["Powfu", "Jomie"]),
+        ]
+    )
