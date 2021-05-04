6beea5486012814b3d6dad05a859ac65d335760c
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-05-04 11:52:58 +0200

    Add comments + remove prints

--- spotify.py
+++ spotify.py
@@ -10,18 +10,18 @@ install()
 def get_spotify_tracks(pid: str = None) -> tuple[list[tuple[str, list[str]]], str]:
     spotify = sp.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-private"))
 
-    print(pid)
-    playlist = spotify.playlist(playlist_id=pid)
-    tracks_objects = spotify.playlist_items(pid, offset=0)["items"]
-    tracks_items = [track["track"] for track in tracks_objects]
-    print(tracks_items)
+    playlist = spotify.playlist(playlist_id=pid)  # Get the playlist object to get the name
+    tracks_objects = spotify.playlist_items(pid, offset=0)["items"]  # Get tracks items
+    tracks_items = [
+        track["track"] for track in tracks_objects  # Omit worthless information for this use case
+    ]
 
-    tracks = [
+    tracks = [  # Create the tuple list made of [(song1 name, [artists]), (song2 name, [artists])]
         (track["name"], [artist["name"] for artist in track["artists"]]) for track in tracks_items
     ]
 
-    return (tracks, playlist["name"])
+    return (tracks, playlist["name"])  # Return all the info in a tuple
 
 
 if __name__ == "__main__":
-    print(get_spotify_tracks("2ThrUxO3KmVj0wQ7hDPD39"))
+    print(get_spotify_tracks("2ThrUxO3KmVj0wQ7hDPD39"))  # Test in a custom private playlist
